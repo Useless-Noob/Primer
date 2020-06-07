0705bb6a1615178cd097eeab971c3ed007ce0cd4
Useless-Noob <60873878+Useless-Noob@users.noreply.github.com>
2020-06-07 23:32:38 +0800

    Update chen_algorithm.h

--- chen_algorithm.h
+++ chen_algorithm.h
@@ -1,1 +1,18 @@
-#include <
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <ios>
+
+void Test()
+{
+  using std::cout;
+  using std::endl;
+  cout << "Hello" << endl;
+  return;
+}
